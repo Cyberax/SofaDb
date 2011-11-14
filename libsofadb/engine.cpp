52c4ca8eb9c449493d8cd3519cff26f5f8585f43
Alex Besogonov <Alex.Besogonov@gmail.com>
2011-11-14 15:19:31 -0500

    Adding the real engine
    Added documentation for the document format in CouchDB

--- libsofadb/engine.cpp
+++ libsofadb/engine.cpp
@@ -1,8 +1,17 @@
 #include "engine.h"
 #include "leveldb/db.h"
+#include <openssl/md5.h>
+#include <boost/array.hpp>
 
 using namespace sofadb;
 using namespace leveldb;
+
+void calculate_hash(std::string str, boost::array<uint8_t, 16> &res)
+{
+	assert(res.size()==MD5_DIGEST_LENGTH);
+	MD5(reinterpret_cast<const unsigned char*>(str.data()), str.size(),
+		res.data());
+}
 
 DbEngine::DbEngine(const std::string &&filename)
 {
--- libsofadb/engine.h
+++ libsofadb/engine.h
@@ -6,6 +6,7 @@
 
 #include <map>
 #include <list>
+#include <boost/optional.hpp>
 
 namespace leveldb {
 	class DB;
@@ -18,25 +19,64 @@ namespace sofadb {
 	class InlineAttachment
 	{
 		std::string name_, content_type_;
-		long long len_;
+		uint8_t md5_[16];
 	};
-	typedef std::map<std::string, InlineAttachment> attachment_map_t;
+	typedef std::vector<InlineAttachment> attachment_vector_t;
 
-	class RevisionInfo
+	struct RevisionInfo
 	{
+		uint32_t id_; //This is a revision number
+		uint8_t md5_[16]; //Document's MD5 hash
+
+		RevisionInfo() : md5_(), id_()
+		{
+		}
 	};
 
+	/**
+		CouchDB keeps documents in the following format:
+		[Deleted, OldStart, OldRev, Body, Atts2] where
+		- 'Deleted' is either 'true' or 'false' atoms
+		- OldStart - the previous revision number as an integer
+		- OldRev - the MD5 of the previous revision, as a binary
+		- Body - the body of the document
+		- Atts2 - inline attachments
+
+		Body has the following structure:
+		- {[{"prop_name", prop_value}, ...]} where prop_name is a binary
+		and prop_value is an Erlang term. This is either a:
+		+ 'null' atom for nulls
+		+ Number/double for integers/doubles
+		+ Binary for string values
+		+ A tuple containing further JSON tuples in the same format.
+
+		Atts2 describes attachments and has the following structure:
+		[{<<file_name>>,<<content_type>>, <<md5>>}, ...] where
+		file_name and content_type are binaries and md5	is a binary with the
+		hash of the content.
+
+		Hash is computed using a braindead algorithm and represents MD5 of the
+		original attachment OR of its zlib-compressed copy. No compression
+		flags are stored, but by default the following documents are compressed
+		by zlib with 8 compression level:
+		text/*, application/javascript, application/json, application/xml
+	  */
 	class DocumentRevision
 	{
 		DbEngine *engine_;
 
-		std::string id_;
-		std::string rev_;
-		bool deleted_;
+		std::string id_; //The immutable document ID
 
-		std::list<std::string> conflicts_;
-		std::list<std::string> deleted_conflicts_;
-		std::list<RevisionInfo> _revs_info;
+		bool deleted_;
+		//Revision in the format num-MD5 where MD5 is a hash of the document's
+		//contents.
+		RevisionInfo previous_rev_;
+		json_spirit::Array body_;
+		attachment_vector_t atts_;
+
+		//Cached revision info, can be computed based on the previous
+		//revision
+		boost::optional<RevisionInfo> rev_;
 	};
 
 	class DbEngine
