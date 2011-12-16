02c346d885240465e86dc7dce996b01127d1a699
Alex Besogonov <Alex.Besogonov@gmail.com>
2011-12-16 01:48:28 -0500

    More skeleton for the socket server

--- server/server_main.cpp
+++ server/server_main.cpp
@@ -2,7 +2,12 @@
 #include <gflags/gflags.h>
 #include <scope_guard.h>
 #include <boost/asio.hpp>
+
+#undef BOOST_HAS_RVALUE_REFS
 #include <boost/thread.hpp>
+#define BOOST_HAS_RVALUE_REFS
+
+#include <iostream>
 
 using boost::asio::local::stream_protocol;
 using namespace sofadb;
@@ -11,6 +16,29 @@ typedef boost::shared_ptr<stream_protocol::socket> socket_ptr_t;
 DEFINE_string(socket_dir, "/tmp", "Listen socket path");
 DEFINE_string(socket_name, "", "Listen socket name");
 DEFINE_int32(socket_backlog, 10, "Maximum socket backlog");
+
+void session(socket_ptr_t sock)
+{
+	try
+	{
+		for (;;)
+		{
+//			char data[max_length];
+
+//			boost::system::error_code error;
+//			size_t length = sock->read_some(boost::asio::buffer(data), error);
+//			if (error == boost::asio::error::eof)
+//				break; // Connection closed cleanly by peer.
+//			else if (error)
+//				throw boost::system::system_error(error); // Some other error.
+
+//			boost::asio::write(*sock, boost::asio::buffer(data, length));
+		}
+	} catch (std::exception& e)
+	{
+		std::cerr << "Exception in thread: " << e.what() << "\n";
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -27,6 +55,8 @@ int main(int argc, char **argv)
 	else
 		socket_file.append(FLAGS_socket_name);
 
+	unlink(socket_file.c_str());
+
 	boost::asio::io_service io_service;
 	stream_protocol::acceptor acceptor(
 				io_service, stream_protocol::endpoint(socket_file));
@@ -37,7 +67,7 @@ int main(int argc, char **argv)
 	{
 		socket_ptr_t sock(new stream_protocol::socket(io_service));
 		acceptor.accept(*sock);
-		//boost::thread t(boost::bind(session, sock));
+		boost::thread t(&session, sock);
 	}
 
 	return 0;
